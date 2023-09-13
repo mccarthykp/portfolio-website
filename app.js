40d2c2883fc4c149e90d9b7dea125c31ab910cc8
Kevin McCarthy <mccarthykp@users.noreply.github.com>
2023-09-12 22:09:27 -0400

    Added Scroll Arrow Anchor w/ Animation

--- app.js
+++ app.js
@@ -34,6 +34,26 @@ function reveal() {
 window.addEventListener('scroll', reveal);
 reveal();
 
+// Disappear & Reveal for Scroll Arrow
+function reverse_reveal() {
+  var disappears = document.querySelectorAll('.reverse_reveal');
+
+  for (var i = 0; i < disappears.length; i++) {
+    var windowHeight = window.innerHeight;
+    var elementTop = disappears[i].getBoundingClientRect().top + 75;
+    var elementVisible = 350;
+    if (elementTop < windowHeight - elementVisible) {
+      disappears[i].classList.add('active');
+    } else {
+      disappears[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener('scroll', reverse_reveal);
+reverse_reveal();
+
+// Typing Header Section
 $("#typed").typed({
   strings: ["Developer", "Designer", "Engineer", "Entrepreneur", "Thinker"],
   typeSpeed: 95,
@@ -44,111 +64,6 @@ $("#typed").typed({
   cursorChar: "|",
   contentType: 'html'
 });
-
-
-// // Mouse Smooth Scroll
-// $(document).ready(function(){
-//   // $fn.scrollSpeed(step, speed, easing);
-//   jQuery.scrollSpeed(200, 800);
-// });
-
-// // Custom scrolling speed with jQuery
-// // Source: github.com/ByNathan/jQuery.scrollSpeed
-// // Version: 1.0.2
-
-// (function($) {
-
-// jQuery.scrollSpeed = function(step, speed, easing) {
-    
-//     var $document = $(document),
-//         $window = $(window),
-//         $body = $('html, body'),
-//         option = easing || 'default',
-//         root = 0,
-//         scroll = false,
-//         scrollY,
-//         scrollX,
-//         view;
-        
-//     if (window.navigator.msPointerEnabled)
-    
-//         return false;
-        
-//     $window.on('mousewheel DOMMouseScroll', function(e) {
-        
-//         var deltaY = e.originalEvent.wheelDeltaY,
-//             detail = e.originalEvent.detail;
-//             scrollY = $document.height() > $window.height();
-//             scrollX = $document.width() > $window.width();
-//             scroll = true;
-        
-//         if (scrollY) {
-            
-//             view = $window.height();
-                
-//             if (deltaY < 0 || detail > 0)
-        
-//                 root = (root + view) >= $document.height() ? root : root += step;
-            
-//             if (deltaY > 0 || detail < 0)
-        
-//                 root = root <= 0 ? 0 : root -= step;
-            
-//             $body.stop().animate({
-        
-//                 scrollTop: root
-            
-//             }, speed, option, function() {
-        
-//                 scroll = false;
-            
-//             });
-//         }
-        
-//         if (scrollX) {
-            
-//             view = $window.width();
-                
-//             if (deltaY < 0 || detail > 0)
-        
-//                 root = (root + view) >= $document.width() ? root : root += step;
-            
-//             if (deltaY > 0 || detail < 0)
-        
-//                 root = root <= 0 ? 0 : root -= step;
-            
-//             $body.stop().animate({
-        
-//                 scrollLeft: root
-            
-//             }, speed, option, function() {
-        
-//                 scroll = false;
-            
-//             });
-//         }
-        
-//         return false;
-        
-//     }).on('scroll', function() {
-        
-//         if (scrollY && !scroll) root = $window.scrollTop();
-//         if (scrollX && !scroll) root = $window.scrollLeft();
-        
-//     }).on('resize', function() {
-        
-//         if (scrollY && !scroll) view = $window.height();
-//         if (scrollX && !scroll) view = $window.width();
-        
-//     });       
-// };
-
-// jQuery.easing.default = function (x,t,b,c,d) {
-
-//     return -c * ((t=t/d-1)*t*t*t - 1) + b;
-// };
-
-// })(jQuery);
 
 // Carousel Animation Pause onHover
 document.addEventListener("DOMContentLoaded", (event) => {
